1c5d5cf76fbe776d8e427cebd2fd3ae5088b1a9f
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2016-02-24 23:47:35 +0300

    Starts as spring boot app

--- src/main/java/com/kuznetsov/parfum/ParfumApplication.java
+++ src/main/java/com/kuznetsov/parfum/ParfumApplication.java
@@ -3,29 +3,13 @@ package com.kuznetsov.parfum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-public class ParfumApplication extends WebMvcConfigurerAdapter {
+@SpringBootApplication
+public class ParfumApplication {
 
-    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
-            "classpath:/META-INF/resources/", "classpath:/resources/",
-            "classpath:/resources/static/", "classpath:/public/" };
     private static final Logger logger = LoggerFactory.getLogger(ParfumApplication.class);
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-    }
     public static void main(String[] args) {
         logger.debug("Started!");
         SpringApplication.run(ParfumApplication.class, args);
