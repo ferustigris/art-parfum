6e34b85c1b5e1589d746e7f35d825fd37d0eeb4f
ferus.tigris@gmail.com <ferus.tigris@gmail.com>
2016-02-24 23:16:37 +0300

    Convert to WAR application. Gets products only for one store

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -29,150 +29,171 @@ function getDaysBetween(d) {
     return parseInt((currentDate.getTime() - d) / (24 * 60 * 60 * 1000));
 }
 
+function loadStore(store) {
+    console.log("Current store ");
+    console.log(store);
+
+    // create fields in the table
+    var fields = [
+        { name: "code", type: "text", width: 100 },
+        { name: "count", type: "number", width: 70 }
+    ];
+
+    for(i = $('#period').val() - 1; i >= 0 ; --i) {
+        var d = getDateInThePast(i)
+        fields.push({ name: "d" + i, title: d.toDateString(), type: "number", width: 70 });
+    };
+
+    $(function() {
+        $( "#tabs" ).tabs({
+            beforeLoad: function( event, ui ) {
+                ui.jqXHR.fail(function() {
+                    ui.panel.html("Couldn't load this tab");
+                });
+            }
+        });
+    });
+
+    fields.push(
+        { name: "balance", type: "number", width: 70 },
+        { type: "control" }
+    );
+
+    // create table
+    $("#jsGrid").jsGrid({
+        width: "100%",
+        height: window.innerHeight - 200,
+
+        filtering: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+        autoload: true,
+
+        data: [],
+        controller: {
+            loadData: function(filter) {
+                filter.store = store;
+                return asyncReceive("/data/products.json", function (result) {
+                    console.log('Products have been received');
+                    var products = $.parseJSON(result);
+                    products.forEach(function(item) {
+                        //stores.forEach(function(store) {
+                            asyncReceive("data/sales.json", function(result) {
+                                var sales = $.parseJSON(result);
+                                sales.forEach(function(sale) {
+                                    var i = getDaysBetween(sale.date);
+                                    item['d' + i] = sale.count;
+                                });
+                                console.log("edit item");
+                                console.log(item);
+                                item.store = store;
+                                $("#jsGrid").jsGrid("insertItem", item);
+                            }, {
+                                'product': item.id,
+                                'date': getDateInThePast(i).getTime(),
+                                'store': store
+                            })
+                        //})
+                    })
+                }, filter)
+            }
+        },
+
+        fields: fields,
+        onItemDeleted: function(args) {
+            console.log("onItemDeleted");
+            asyncReceive("/data/removeProduct.json", function () {}, args.item);
+        },
+        onItemUpdated: function(args) {
+            console.log("onItemUpdated");
+            asyncReceive("/data/updateProduct.json", function () {
+                for(i = 0; i < $('#period').val(); ++i) {
+                    asyncReceive("/data/updateSale.json", function () {
+                        console.log("sale updated");
+                    }, {
+                        'product': args.item.id,
+                        'store': args.item.store,
+                        'day': i,
+                        'count': args.item['d' + i]
+                    });
+                }
+            }, args.item);
+        }
+    });
+
+    dialog = $("#detailsDialog").dialog({
+        autoOpen: false,
+        width: 400,
+        height: 350,
+        modal: true,
+        buttons: {
+          "Create an product": function() {
+            item = {
+                name: $("#new-product-name").val(),
+                code: $("#new-product-code").val(),
+                count: parseInt($("#new-product-count").val()),
+                store: parseInt($("#new-product-store").val())
+            }
+
+            console.log("Try to create new product...")
+            console.log(item)
+
+            asyncReceive("/data/addNewProduct.json", addedProductReceive, item)
+          },
+          Cancel: function() {
+            dialog.dialog( "close" );
+          }
+        },
+        close: function() {
+            $("#detailsForm").validate().resetForm();
+            $("#detailsForm").find(".error").removeClass("error");
+        }
+    });
+
+    $( "#add-new-product" ).button().on( "click", function() {
+        dialog.dialog( "open" );
+    });
+
+    $("#detailsForm").validate({
+        rules: {
+            name: "required",
+            code: { required: true, minlength: 2 },
+            count: { required: true, range: [0, 1500] },
+            store: "required"
+        },
+        messages: {
+            name: "Please enter name",
+            code: "Please enter valid code",
+            count: "Please enter count",
+            store: "Please select store"
+        },
+        submitHandler: function() {
+            console.log("Try to submit")
+        }
+    });
+}
+
 $(document).ready(function () {
 
     asyncReceive("data/stores.json", function (result) {
         console.log('Stores have been received')
         var stores = $.parseJSON(result);
 
-        // create fields in the table
-        var fields = [
-            { name: "code", type: "text", width: 100 },
-            { name: "count", type: "number", width: 70 }
-        ];
-
-        for(i = $('#period').val() - 1; i >= 0 ; --i) {
-            var d = getDateInThePast(i)
-            fields.push({ name: "d" + i, title: d.toDateString(), type: "number", width: 70 });
-        };
-
-        $(function() {
-            $( "#tabs" ).tabs({
-                beforeLoad: function( event, ui ) {
-                    ui.jqXHR.fail(function() {
-                        ui.panel.html("Couldn't load this tab");
-                    });
-                }
-            });
+        $('#period').selectmenu();
+        $('#storesEl');
+        var storesEl = $("#storesEl");
+        storesEl.empty();
+        stores.forEach(function(store) {
+            storesEl.append('<option value="' + store.id + '">' + store.name + '</option>');
         });
-
-        fields.push(
-            { name: "balance", type: "number", width: 70 },
-            { name: "store", type: "select", items: stores, valueField: "id", textField: "name" },
-            { type: "control" }
-        );
-
-        // create table
-        $("#jsGrid").jsGrid({
-            width: "100%",
-            height: window.innerHeight - 200,
-
-            filtering: true,
-            editing: true,
-            sorting: true,
-            paging: true,
-            autoload: true,
-
-            data: [],
-            controller: {
-                loadData: function(filter) {
-                    return asyncReceive("/data/products.json", function (result) {
-                        console.log('Products have been received')
-                        var products = $.parseJSON(result);
-                        products.forEach(function(item) {
-                            stores.forEach(function(store) {
-                                item.store = store.id;
-                                asyncReceive("data/sales.json", function(result) {
-                                    var sales = $.parseJSON(result);
-                                    sales.forEach(function(sale) {
-                                        var i = getDaysBetween(sale.date);
-                                        item['d' + i] = sale.count;
-                                    });
-                                    console.log("edit item");
-                                    console.log(item);
-                                    $("#jsGrid").jsGrid("insertItem", item);
-                                }, {
-                                    'product': item.id,
-                                    'date': getDateInThePast(i).getTime(),
-                                    'store': item.store
-                                })
-                            })
-                        })
-                    }, filter)
-                }
-            },
-
-            fields: fields,
-            onItemDeleted: function(args) {
-                console.log("onItemDeleted");
-                asyncReceive("/data/removeProduct.json", function () {}, args.item);
-            },
-            onItemUpdated: function(args) {
-                console.log("onItemUpdated");
-                asyncReceive("/data/updateProduct.json", function () {
-                    for(i = 0; i < $('#period').val(); ++i) {
-                        asyncReceive("/data/updateSale.json", function () {
-                            console.log("sale updated");
-                        }, {
-                            'product': args.item.id,
-                            'store': args.item.store,
-                            'day': i,
-                            'count': args.item['d' + i]
-                        });
-                    }
-                }, args.item);
+        storesEl.selectmenu({
+            change: function( event, data ) {
+                console.log("loadStore");
+                loadStore(data.item.value);
             }
         });
 
-        dialog = $("#detailsDialog").dialog({
-            autoOpen: false,
-            width: 400,
-            height: 350,
-            modal: true,
-            buttons: {
-              "Create an product": function() {
-                item = {
-                    name: $("#new-product-name").val(),
-                    code: $("#new-product-code").val(),
-                    count: parseInt($("#new-product-count").val()),
-                    store: parseInt($("#new-product-store").val())
-                }
-
-                console.log("Try to create new product...")
-                console.log(item)
-
-                asyncReceive("/data/addNewProduct.json", addedProductReceive, item)
-              },
-              Cancel: function() {
-                dialog.dialog( "close" );
-              }
-            },
-            close: function() {
-                $("#detailsForm").validate().resetForm();
-                $("#detailsForm").find(".error").removeClass("error");
-            }
-        });
-
-        $( "#add-new-product" ).button().on( "click", function() {
-            dialog.dialog( "open" );
-        });
-
-        $("#detailsForm").validate({
-            rules: {
-                name: "required",
-                code: { required: true, minlength: 2 },
-                count: { required: true, range: [0, 1500] },
-                store: "required"
-            },
-            messages: {
-                name: "Please enter name",
-                code: "Please enter valid code",
-                count: "Please enter count",
-                store: "Please select store"
-            },
-            submitHandler: function() {
-                console.log("Try to submit")
-            }
-        });
+        loadStore(stores[0].id);
     });
 });
